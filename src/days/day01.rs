9f2576e22fd42cc0020ae952a93e2cd9750ea986
Sebastian Hew <jingfee@gmail.com>
2020-11-16 20:27:27 +0100

    add test to day1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -18,31 +18,74 @@ impl Solver for Problem {
     }
 
     fn solve_part_one(&self, input: &Vec<char>) -> i32 {
-        let mut level = 0;
-        for instruction in input.iter() {
-            if *instruction == '(' {
-                level = level + 1;
-            } else if *instruction == ')' {
-                level = level - 1;
-            }
-        }
-        return level;
+        get_floor(&input)
     }
 
     fn solve_part_two(&self, input: &Vec<char>) -> i32 {
-        let mut level = 0;
-        for (index, instruction) in input.iter().enumerate() {
-            if *instruction == '(' {
-                level = level + 1;
-            } else if *instruction == ')' {
-                level = level - 1;
-            }
+        get_basement_position(input)
+    }
+}
 
-            if level == -1 {
-                return index as i32 + 1;
-            }
+fn get_floor(input: &Vec<char>) -> i32 {
+    let mut level = 0;
+    for instruction in input.iter() {
+        if *instruction == '(' {
+            level = level + 1;
+        } else if *instruction == ')' {
+            level = level - 1;
+        }
+    }
+    level
+}
+
+fn get_basement_position(input: &Vec<char>) -> i32 {
+    let mut level = 0;
+    for (index, instruction) in input.iter().enumerate() {
+        if *instruction == '(' {
+            level = level + 1;
+        } else if *instruction == ')' {
+            level = level - 1;
         }
 
-        -1
+        if level == -1 {
+            return index as i32 + 1;
+        }
+    }
+
+    -1
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::days::day01::*;
+
+    #[test]
+    fn test_get_floor() {
+        let ex1: Vec<char> = "(())".chars().collect();
+        let ex2: Vec<char> = "(((".chars().collect();
+        let ex3: Vec<char> = "(()(()(".chars().collect();
+        let ex4: Vec<char> = "))(((((".chars().collect();
+        let ex5: Vec<char> = "())".chars().collect();
+        let ex6: Vec<char> = "))(".chars().collect();
+        let ex7: Vec<char> = ")))".chars().collect();
+        let ex8: Vec<char> = ")())())".chars().collect();
+
+        assert_eq!(get_floor(&ex1), 0);
+        assert_eq!(get_floor(&ex2), 3);
+        assert_eq!(get_floor(&ex3), 3);
+        assert_eq!(get_floor(&ex4), 3);
+        assert_eq!(get_floor(&ex5), -1);
+        assert_eq!(get_floor(&ex6), -1);
+        assert_eq!(get_floor(&ex7), -3);
+        assert_eq!(get_floor(&ex8), -3);
+    }
+
+    #[test]
+    fn test_get_basement_position() {
+        let ex1: Vec<char> = ")".chars().collect();
+        let ex2: Vec<char> = "()())".chars().collect();
+
+        assert_eq!(get_basement_position(&ex1), 1);
+        assert_eq!(get_basement_position(&ex2), 5);
     }
 }
